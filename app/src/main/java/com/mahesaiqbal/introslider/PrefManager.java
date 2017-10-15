a247da7ea87d44feabaca7388035dfdb586500d1
mahesaiqbal <iqbalridwansyah.ir@gmail.com>
2017-10-15 22:48:03 +0700

    Initial commit

---
+++ app/src/main/java/com/mahesaiqbal/introslider/PrefManager.java
@@ -0,0 +1,44 @@
+package com.mahesaiqbal.introslider;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by mahesaiqbal on 10/8/2017.
+ */
+
+public class PrefManager {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // Shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "tajwid-welcome";
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+
+    }
+
+    public boolean isFirstTimeLaunch() {
+
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+
+    }
+
+}
